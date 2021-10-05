9f60cf62e4bf497c98b0766c5ac01da3f7da5c17
David Grinberg <DVGrinberg@sberbank.ru>
2021-10-05 20:18:29 +0300

    update docs for watcher, add yolo trained from scratch

--- app/camera/watcher.py
+++ app/camera/watcher.py
@@ -2,11 +2,12 @@ import cv2
 import torch
 
 model = torch.hub.load('ultralytics/yolov5',
-                       'custom',
-                       path='models/yolov5s_987.pt')
-# # model = torch.hub.load('ultralytics/yolov5',
-# #                        'yolov5s',
-# #                        pretrained=True)
+                       'custom', force_reload=True,
+                       path='models/yolov5s_955_scratch.pt')
+
+# model = torch.hub.load('ultralytics/yolov5',
+#                        'yolov5s',
+#                        pretrained=True)
 model.eval()
 for p in model.parameters():
     p.requires_grad = False
@@ -31,8 +32,6 @@ while cap.isOpened():
 
     for i, d in enumerate(dets.xyxy[0]):
         x1, y1, x2, y2, conf, label = d
-        if conf < 0.5:
-            continue
 
         x1 = int(x1)
         y1 = int(y1)
@@ -46,9 +45,10 @@ while cap.isOpened():
                       color=(255, 0, 0),
                       lineType=cv2.LINE_AA)
 
-        cv2.putText(img_result, dets.names[int(d[-1])], (50, 50 + 50 * i),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
-                    cv2.LINE_AA)
+        cv2.putText(img_result,
+                    f"{dets.names[int(d[-1])]}, confidence={conf:.3f}",
+                    (50, 50 + 50 * i), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (255, 255, 255), 2, cv2.LINE_AA)
 
     cv2.imshow('result', img_result)
     if cv2.waitKey(1) == ord('q'):
